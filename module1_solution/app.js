9b365c2dbe6c3b050bf14b5da0cea5cd170afb22
Pavan Mullapudi <pavank.mullapudi@gmail.com>
2017-02-10 03:43:47 -0500

    module1 assignment solution

---
+++ module1_solution/app.js
@@ -0,0 +1,33 @@
+(function () {
+
+  angular.module("Module1Assignemt", [])
+  .controller("Module1AssignemtController", Module1AssignemtController);
+
+  Module1AssignemtController.$inject = ["$scope"];
+  function Module1AssignemtController ($scope) {
+
+    $scope.lunchItems = "";
+    $scope.outPutMessage = "";
+
+    $scope.resetMessage = function () {
+      $scope.outPutMessage = "";
+    };
+
+    $scope.checkLunchItems = function () {
+      var lunitemsArray = $scope.lunchItems.split(',');
+
+      if ($scope.lunchItems.length == 0) {
+        $scope.outPutMessage = "Please enter data first";
+      }
+      else if(lunitemsArray.length <= 3) {
+          $scope.outPutMessage = "Enjoy!";
+      }
+      else {
+        $scope.outPutMessage = "Too much!";
+        $scope.fontColor = "green";
+      }
+
+    };
+    
+  }
+})();
