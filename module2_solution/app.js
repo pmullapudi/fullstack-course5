49cb49f42a6031419dca94ebec07019d60ff752a
Pavan Mullapudi <pavank.mullapudi@gmail.com>
2017-02-14 02:56:42 -0500

    updated

--- module2_solution/app.js
+++ module2_solution/app.js
@@ -1,2 +1,15 @@
 
-//starter code
+//starter code ShoppingListCheckOff
+//ToBuyController
+//AlreadyBoughtController
+
+//ShoppingListCheckOffService
+
+
+(function () {
+  'use strict';
+
+  angular.module('ShoppingListCheckOff', []);
+
+
+})();
