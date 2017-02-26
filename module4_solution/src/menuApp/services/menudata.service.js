f3a98943a120eb534f9b99fcb5563a832f12e1d8
Pavan Mullapudi <pavank.mullapudi@gmail.com>
2017-02-26 01:22:32 -0500

    updated

--- module4_solution/src/menuApp/services/menudata.service.js
+++ module4_solution/src/menuApp/services/menudata.service.js
@@ -1,26 +1,34 @@
 (function () {
   'use strict';
 
-// MenuDataService  Service
-
-// Create menudata.service.js file and create a service called MenuDataService in it. This service should be declared on the data module, not on the MenuApp module. The MenuDataService should have 2 methods:
-//
-//     getAllCategories - this method should return a promise which is a result of using the $http service, using the following REST API endpoint: https://davids-restaurant.herokuapp.com/categories.json
-//     getItemsForCategory(categoryShortName) - this method should return a promise which is a result of using the $http service, using the following REST API endpoint: https://davids-restaurant.herokuapp.com/menu_items.json?category=, where, before the call to the server, your code should append whatever categoryShortName value was passed in as an argument into the getItemsForCategory method.
-
-    angular.module('data')
-    .service('MenuDataService', MenuDataService);
+  angular.module('data')
+    .service('MenuDataService', MenuDataService)
+    .constant('ApiBaseURL', 'https://davids-restaurant.herokuapp.com');
 
 
-    MenuDataService.$inject = ['$q'];
-    function MenuDataService($q) {
+  MenuDataService.$inject = ['$http', 'ApiBaseURL'];
+  function MenuDataService($http, ApiBaseURL) {
 
-      var menuDataService = this;
+    var menuDataService = this;
 
-      menuDataService.getAllCategories = function () {};
+    menuDataService.getAllCategories = function () {
+      var response = $http({
+        method: "GET",
+        url: (ApiBaseURL + '/categories.json')
+      });
 
-      menuDataService.getItemsForCategory = function (categoryShortName) {};
+      return response;
+    };
 
-    }
+    menuDataService.getItemsForCategory = function (categoryShortName) {
+      var response = $http({
+        method: "GET",
+        url: (ApiBaseURL + '/menu_items.json?category=' + categoryShortName)
+      });
+
+      return response;
+    };
+
+  }
 
 })();
