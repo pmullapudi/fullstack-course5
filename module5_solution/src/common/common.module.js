28e07ece49e0063f9f51eb0c0ad431d1d29f234f
Pavan Mullapudi <pavank.mullapudi@gmail.com>
2017-03-03 04:59:27 -0500

    module5 solution

--- module5_solution/src/common/common.module.js
+++ module5_solution/src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'http://pmullapudi-course5.herokuapp.com')
+.constant('ApiPath', 'https://pmullapudi-course5.herokuapp.com')
 .config(config);
 
 config.$inject = ['$httpProvider'];
